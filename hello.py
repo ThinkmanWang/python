26f029930044d58995e7c63e5efd9dc34690bae1
root <root@localhost.localdomain>
2016-06-16 10:24:53 +0300

    update

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-if __name__ == '__main__':  
-    app.run(host='0.0.0.1', port=8000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
 
-    
+
