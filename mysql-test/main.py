c95314bc28970498a0d755ec381e648864aeca61
Thinkman Wang <thinkman_wang@azhuoinfo.com>
2016-06-18 01:08:16 +0800

    add token success

--- mysql-test/main.py
+++ mysql-test/main.py
@@ -7,7 +7,9 @@ from models.User import User
 from utils.user_db_utils import *  
 import json
 from utils.object2json import obj2json
-    
+import hashlib
+import time
+
 def main():
     print 'Hello World'  
     myDB = MysqlPython('thinkman-wang.com', 'thinkman', 'Ab123456', 'db_thinknews')
@@ -37,6 +39,10 @@ def main():
         print obj2json(user)
     else:
         print("login failed")
+        
+    szToken = ("%s%d" % ("a0a475cf454cf9a06979034098167b9e", int(time.time())))
+    print(hashlib.md5(szToken).hexdigest())
+    
     
 if __name__ == '__main__': 
     main();
