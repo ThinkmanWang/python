38c288b8c0d96c5395a503875dc61d49be8c4a0d
Thinkman Wang <wangxf1985@gmail.com>
2016-06-17 14:46:28 +0300

    remove logs

--- mysql-test/utils/user_db_utils.py
+++ mysql-test/utils/user_db_utils.py
@@ -3,7 +3,7 @@ import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'models'))
 
-print(sys.path)
+#print(sys.path)
 
 from mysql_python import MysqlPython
 from models.User import User
@@ -13,7 +13,7 @@ from DBUtils.PooledDB import PooledDB
 
 def get_all_users() :
     myDB = MysqlPython('thinkman-wang.com', 'thinkman', 'Ab123456', 'db_thinknews')
-    
+
     szSql = 'select * from user'
     result = myDB.select_advanced(szSql)
 
@@ -22,19 +22,19 @@ def get_all_users() :
         user = User()
         user.id, user.user_name, user.password = obj
         lstUser.append(user)
-        
+
     return lstUser
-        
+
 g_dbPool = PooledDB(MySQLdb, 5, host='thinkman-wang.com', user='thinkman', passwd='Ab123456', db='db_thinknews', port=3306);
 
-def get_all_user_from_pool():    
+def get_all_user_from_pool():
     conn = g_dbPool.connection()
     cur=conn.cursor()
-    SQL="select * from user"  
+    SQL="select * from user"
     cur.execute(SQL)
-    
+
     rows=cur.fetchall()
-    
+
     lstUser = []
     for row in rows:
         user = User()
@@ -42,10 +42,10 @@ def get_all_user_from_pool():
         user.user_name = row[1]
         user.password = row[2]
         lstUser.append(user)
-    
+
     cur.close()
-    conn.close()    
+    conn.close()
     return lstUser
-    
-    
-    
+
+
+
